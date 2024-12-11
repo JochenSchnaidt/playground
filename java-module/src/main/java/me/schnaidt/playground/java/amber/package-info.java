41b9746a4f215f8d3390e769dbef4229bd5d92d4
jschnaidt <jochen.schnaidt@prodyna.com>
2024-12-11 10:52:37 +0100

    Added package info

---
+++ java-module/src/main/java/me/schnaidt/playground/java/amber/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * This package is dedicated to project amber.
+ * <p>
+ * The goal of Project Amber is to explore and incubate smaller, productivity-oriented Java language features to make the development process nicer.<br>
+ * See following links for further information.
+ * </p>
+ * <ul>
+ *     <li>Official documentation at <a href="https://openjdk.org/projects/amber/">OpenJDK</a></li>
+ *     <li>Introduction to Project Amber on <a href="https://www.baeldung.com/java-project-amber">Baeldung</a></li>
+ *     <li>General feature overview on <a href="https://javaalmanac.io">Java Version Almanac</a></li>
+ * </ul>
+ */
+package me.schnaidt.playground.java.amber;
